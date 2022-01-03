d9d00feb840cca11f21222fd3ac9330a10973bb6
Yusuf Tran <mumblez@users.noreply.github.com>
2022-01-03 17:14:52 +0000

    initial commit of new nvim lua config

---
+++ nvim-lua/lua/user/keymaps.lua
@@ -0,0 +1,77 @@
+local opts = { noremap = true, silent = true }
+
+local term_opts = { silent = true }
+
+-- Shorten function name
+local keymap = vim.api.nvim_set_keymap
+
+--Remap comma as leader key
+keymap("", ",", "<Nop>", opts)
+vim.g.mapleader = ","
+vim.g.maplocalleader = ","
+
+-- Modes
+--   normal_mode = "n",
+--   insert_mode = "i",
+--   visual_mode = "v",
+--   visual_block_mode = "x",
+--   term_mode = "t",
+--   command_mode = "c",
+
+-- Normal --
+-- Better window navigation
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
+
+-- Resize with arrows
+-- keymap("n", "<C-Up>", ":resize -2<CR>", opts)
+-- keymap("n", "<C-Down>", ":resize +2<CR>", opts)
+-- keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+-- keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+
+-- Move text up and down
+-- keymap("n", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
+-- keymap("n", "<A-k>", "<Esc>:m .-2<CR>==gi", opts)
+
+-- Insert --
+-- Press jk fast to enter
+-- keymap("i", "jk", "<ESC>", opts)
+
+-- Move text up and down
+-- keymap("v", "<A-j>", ":m .+1<CR>==", opts)
+-- keymap("v", "<A-k>", ":m .-2<CR>==", opts)
+-- keymap("v", "p", '"_dP', opts)
+
+-- Visual Block --
+-- Move text up and down
+-- keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
+-- keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
+-- keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
+-- keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
+
+-- Terminal --
+-- Better terminal navigation
+-- keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
+-- keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
+-- keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
+-- keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
+
+-- Navigate buffers
+keymap("n", "<c-n>", ":bnext<CR>", opts)
+keymap("n", "<c-p>", ":bprevious<CR>", opts)
+
+-- Stay in indent mode
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
+
+
+keymap("n", "<leader>h", ":nohlsearch<CR>", opts)           -- toggle highlighted search
+keymap("n", "<leader>q", ":quit<CR>", opts)                 -- quick quit
+keymap("n", "<leader>Q", ":qa!<CR>", opts)
+keymap("n", "<leader>o", "o<ESC>k", opts)                   -- add blank lines above and below current line.
+keymap("n", "<leader>O", "O<ESC>j", opts)
+keymap("c", "w!!", "w !sudo tee > /dev/null %", opts)       -- sudo save
+keymap("n", "<leader>m", ":MaximizerToggle!", opts)         -- window maximize
+
