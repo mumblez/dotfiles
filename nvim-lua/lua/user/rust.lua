ebee1d0f928eb2b9b60ac62740307ccc04caf7eb
Yusuf Tran <mumblez@users.noreply.github.com>
2022-01-04 12:36:00 +0000

    cleanup

--- nvim-lua/lua/user/rust.lua
+++
@@ -1,4 +0,0 @@
--- Enable type inlay hints
-vim.cmd [[
-    autocmd InsertLeave,BufEnter,BufWinEnter,TabEnter,BufWritePost *.rs :lua require'lsp_extensions'.inlay_hints{ prefix = '', highlight = "Comment", enabled = {"TypeHint", "ChainingHint", "ParameterHint"} }
-]]
