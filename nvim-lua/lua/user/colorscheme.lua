1a56421dce18f849699b827b34d068b522b9e8c6
Yusuf Tran <mumblez@users.noreply.github.com>
2022-01-07 16:45:20 +0000

    tweak nightfox theme settings

--- nvim-lua/lua/user/colorscheme.lua
+++ nvim-lua/lua/user/colorscheme.lua
@@ -8,6 +8,7 @@
 -- endtry
 -- ]]
 
+-- local colorscheme = "darcula-solid"
 local colorscheme = "nightfox"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
@@ -15,4 +16,25 @@ if not status_ok then
     vim.notify("colorscheme " .. colorscheme .. " not found!")
 end
 
-require('nightfox').load('nightfox')
+local nf = require('nightfox')
+
+nf.setup({
+  fox = "nightfox",
+  styles = {
+    comments = "italic", -- change style of comments to be italic
+    keywords = "bold", -- change style of keywords to be bold
+    functions = "italic,bold" -- styles can be a comma separated list
+  },
+  inverse = {
+    match_paren = true, -- inverse the highlighting of match_parens
+  },
+  colors = {
+    red = "#FF000", -- Override the red color for MAX POWER
+    bg_alt = "#000000",
+  },
+  hlgroups = {
+    TSPunctDelimiter = { fg = "${red}" }, -- Override a highlight group with the color red
+    LspCodeLens = { bg = "#000000", style = "italic" },
+  }
+})
+nf.load()
