5a82a96aeed71673a6d8ab4d5b6430d7d798275e
Yusuf Tran <mumblez@users.noreply.github.com>
2022-05-23 18:33:39 +0100

    update and lazily fix lua

--- nvim-lua/lua/user/colorscheme.lua
+++ nvim-lua/lua/user/colorscheme.lua
@@ -12,32 +12,37 @@
 -- local colorscheme = "darcula-solid"
 local colorscheme = "nightfox"
 
+
+local nf = require('nightfox')
+nf.setup({
+  -- fox = "nightfox",
+  options = {
+    styles = {
+      comments = "italic", -- change style of comments to be italic
+      keywords = "bold", -- change style of keywords to be bold
+      functions = "italic,bold" -- styles can be a comma separated list
+    },
+    inverse = {
+      match_paren = true, -- inverse the highlighting of match_parens
+    }
+  },
+  -- colors = {
+  -- pallettes = {
+  --   red = "#FF000", -- Override the red color for MAX POWER
+  --   bg_alt = "#000000",
+  -- },
+  -- -- hlgroups = {
+  -- groups = {
+  --   TSPunctDelimiter = { fg = "${red}" }, -- Override a highlight group with the color red
+  --   LspCodeLens = { bg = "#000000", style = "italic" },
+  -- }
+})
+--nf.load()
+
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
     vim.notify("colorscheme " .. colorscheme .. " not found!")
 end
-
-local nf = require('nightfox')
-nf.setup({
-  fox = "nightfox",
-  styles = {
-    comments = "italic", -- change style of comments to be italic
-    keywords = "bold", -- change style of keywords to be bold
-    functions = "italic,bold" -- styles can be a comma separated list
-  },
-  inverse = {
-    match_paren = true, -- inverse the highlighting of match_parens
-  },
-  colors = {
-    red = "#FF000", -- Override the red color for MAX POWER
-    bg_alt = "#000000",
-  },
-  hlgroups = {
-    TSPunctDelimiter = { fg = "${red}" }, -- Override a highlight group with the color red
-    LspCodeLens = { bg = "#000000", style = "italic" },
-  }
-})
-nf.load()
 
 -- show column boundary when we get there
 -- vim.cmd [[
